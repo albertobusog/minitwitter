1d465bc82fe9729ee51135eaeb49da7b1c9f0a3a
albertobusog <alberto.buso@thincode.com>
2021-11-04 23:58:24 -0600

    photo test update

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -40,4 +40,80 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  
+  describe "GET /users/:id - show action" do
+    subject do
+      get "/users/#{user_id}", headers: { ACCEPT: 'application/json' }
+      response
+    end
+
+    let(:user) do
+      FactoryBot.create(:user)
+      # FactoryBot.create(:user, username: 'juan', full_name: 'Juan Perez')
+    end
+
+    context 'the user does not exist' do
+      let(:user_id) { 0 }
+      it { expect(subject.parsed_body).to include({ "error" => 'User not found' }) }
+    end
+
+    context 'the user exists' do
+      let(:user_id) { user.id }
+      it do
+        expect(subject.parsed_body).to include({
+          "username" => "tony",
+          "full_name" => "Tony Stark",
+        })
+      end
+    end
+  end
+
+  describe "POST /users - create action" do
+    subject do
+      post "/users", headers: { ACCEPT: 'application/json' }, params: params
+      response
+    end
+
+    context 'the user is not created' do
+      let(:params) do
+        {
+          user: {
+            username: 's',
+            password: '123456',
+            password_confirmation: '123456',
+            full_name: 'Steve Rogers',
+          }
+        }
+      end
+
+      it { expect(subject.parsed_body).to include('Username is too short (minimum is 2 characters)') }
+    end
+
+    context 'the user is created correctly' do
+      let(:params) do
+        {
+          user: {
+            username: 'steve',
+            password: '123456',
+            password_confirmation: '123456',
+            full_name: 'Steve Rogers',
+          }
+        }
+      end
+
+      it do
+        expect{ subject }.to change { User.first }.from(nil).to have_attributes({
+          username: 'steve',
+          full_name: 'Steve Rogers',
+        })
+        expect(subject.parsed_body).to include({
+          "username" => "steve",
+          "full_name" => "Steve Rogers",
+        })
+
+      end
+    end
+  end
+
 end
